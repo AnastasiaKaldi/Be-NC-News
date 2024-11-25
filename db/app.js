b81c6540da9d380d2724b14003e39ca8e907a342
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-11-25 12:53:57 +0000

    Task 3

--- db/app.js
+++ db/app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const fs = require("fs");
 const endpointsJson = require("../endpoints.json");
+const topics = require("../db/data/test-data/topics");
 
 const app = express();
 
 app.get("/api", (req, res) => {
   res.status(200).json({ endpoints: endpointsJson });
+});
+
+app.get("/api/topics", (req, res) => {
+  res.status(200).json({ topics });
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(err.status || 500).json({
+      error: {
+        message: err.message || "Internal Server Error",
+      },
+    });
+  }
 });
 
 module.exports = app;
