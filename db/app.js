066437bba6628ef29e7e04e31a2b83f66af1e438
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-11-28 16:38:35 +0000

    task 7

--- db/app.js
+++ db/app.js
@@ -8,9 +8,12 @@ const { getArticles } = require("../db/controller/articles.controller");
 const {
   getCommentsByArticleId,
 } = require("../db/controller/comments.controller");
+const {
+  postCommentByArticleId,
+} = require("../db/controller/commentsId.controller");
 
 const app = express();
-
+app.use(express.json());
 app.get("/api", (req, res) => {
   res.status(200).json({ endpoints: endpointsJson });
 });
@@ -22,8 +25,17 @@ app.get("/api/articles", getArticles);
 
 app.get("/api/:article_id/comments", getCommentsByArticleId);
 
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
+
 app.use((err, req, res, next) => {
-  console.error(err);
+  console.error("Error Middleware Triggered:", err);
+  console.error("Error Stack:", err.stack);
   res.status(err.status || 500).json({
     error: {
       message: err.message || "An unknown error occurred",
