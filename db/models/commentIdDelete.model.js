0a52d7111c258ba92fa2b1897995908a54d77c5e
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-11-29 10:27:26 +0000

    Test

--- db/models/commentIdDelete.model.js
+++
@@ -1,9 +0,0 @@
-const db = require("../connection");
-
-exports.deleteCommentById = (comment_id) => {
-  const queryStr = `
-    DELETE FROM comments
-    WHERE comment_id = $1;
-  `;
-  return db.query(queryStr, [comment_id]).then(({ rowCount }) => rowCount);
-};
