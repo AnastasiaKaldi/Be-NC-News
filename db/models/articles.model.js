9697ff898212e953a4d23bbb23286221ea42bedc
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-12-18 10:43:10 +0000

    Fix6

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -35,36 +35,47 @@
 //   return Promise.resolve(articles);
 // };
 
+// const articles = require("../data/test-data/articles");
+// const comments = require("../data/test-data/comments");
+
+// exports.fetchArticles = () => {
+//   return Promise.resolve(
+//     articles
+//       .map((article) => {
+//         const articleCommentsCount = comments.filter(
+//           (comment) => comment.article_id === article.article_id
+//         ).length;
+//         const { body, ...articleWithoutBody } = article;
+//         return {
+//           ...articleWithoutBody,
+//           comment_count: articleCommentsCount,
+//         };
+//       })
+//       .sort((a, b) => b.created_at - a.created_at)
+//   );
+// };
+
 const articles = require("../data/test-data/articles");
 const comments = require("../data/test-data/comments");
 
-exports.fetchArticles = async () => {
-  try {
-    const query = `
-      SELECT 
-        articles.article_id,
-        articles.author,
-        articles.title,
-        articles.topic,
-        articles.created_at,
-        articles.votes,
-        articles.article_img_url,
-        COUNT(comments.comment_id) AS comment_count
-      FROM articles
-      LEFT JOIN comments
-      ON articles.article_id = comments.article_id
-      GROUP BY articles.article_id
-      ORDER BY articles.created_at DESC;
-    `;
+exports.fetchArticles = () => {
+  const articlesWithIds = articles.map((article, index) => ({
+    article_id: index + 1,
+    ...article,
+  }));
+  const articlesWithCommentCount = articlesWithIds.map((article) => {
+    const articleCommentsCount = comments.filter(
+      (comment) => comment.article_id === article.article_id
+    ).length;
 
-    const result = await pool.query(query);
+    const { body, ...articleWithoutBody } = article;
 
-    return result.rows.map((article) => ({
-      ...article,
-      comment_count: parseInt(article.comment_count, 10),
-    }));
-  } catch (error) {
-    console.error("Error fetching articles with comment count:", error);
-    throw error;
-  }
+    return {
+      ...articleWithoutBody,
+      comment_count: articleCommentsCount,
+    };
+  });
+  return Promise.resolve(
+    articlesWithCommentCount.sort((a, b) => b.created_at - a.created_at)
+  );
 };
