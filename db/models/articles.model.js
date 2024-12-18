9db9f02683e7cc865820d513ff0aebb38c47a63c
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-12-18 09:18:23 +0000

    Fix4

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -38,19 +38,33 @@
 const articles = require("../data/test-data/articles");
 const comments = require("../data/test-data/comments");
 
-exports.fetchArticles = () => {
-  return Promise.resolve(
-    articles
-      .map((article) => {
-        const articleCommentsCount = comments.filter(
-          (comment) => comment.article_id === article.article_id
-        ).length;
-        const { body, ...articleWithoutBody } = article;
-        return {
-          ...articleWithoutBody,
-          comment_count: articleCommentsCount,
-        };
-      })
-      .sort((a, b) => b.created_at - a.created_at)
-  );
+exports.fetchArticles = async () => {
+  try {
+    const query = `
+      SELECT 
+        articles.article_id,
+        articles.author,
+        articles.title,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id) AS comment_count
+      FROM articles
+      LEFT JOIN comments
+      ON articles.article_id = comments.article_id
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC;
+    `;
+
+    const result = await pool.query(query);
+
+    return result.rows.map((article) => ({
+      ...article,
+      comment_count: parseInt(article.comment_count, 10),
+    }));
+  } catch (error) {
+    console.error("Error fetching articles with comment count:", error);
+    throw error;
+  }
 };
