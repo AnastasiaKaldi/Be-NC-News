688c1379a8c3a4e33e2f82965cedc2e90028eedc
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-12-19 16:14:51 +0000

    Fix7

--- db/controller/articlesIdPatch.controller.js
+++ db/controller/articlesIdPatch.controller.js
@@ -4,15 +4,20 @@ exports.patchArticleById = (req, res, next) => {
   const { article_id } = req.params;
   const { votes } = req.body;
 
+  console.log("Request params:", req.params); // Log params
+  console.log("Request body:", req.body); // Log body
+
   updateArticleById(article_id, votes)
     .then((updatedArticle) => {
       if (!updatedArticle) {
+        console.error("Article not found during update");
         return next({ status: 404, message: "Article not found" });
       }
+      console.log("Updated article:", updatedArticle);
       res.status(200).json({ article: updatedArticle });
     })
     .catch((err) => {
       console.error("Error updating article:", err);
-      next({ status: 500, message: "Failed to update article" });
+      next(err);
     });
 };
--- db/models/articlesIdPatch.model.js
+++ db/models/articlesIdPatch.model.js
@@ -1,7 +1,11 @@
 const articles = require("../data/test-data/articles");
 
 exports.updateArticleById = (article_id, votes) => {
+  console.log("article_id:", article_id);
+  console.log("votes:", votes);
+
   if (!votes || typeof votes !== "number") {
+    console.error("Votes validation failed");
     return Promise.reject({
       status: 400,
       message: "Request body must include 'votes' as a number",
@@ -13,14 +17,18 @@ exports.updateArticleById = (article_id, votes) => {
     ...article,
   }));
 
+  console.log("Mapped articles:", articlesWithIds);
+
   const article = articlesWithIds.find(
     (article) => article.article_id === Number(article_id)
   );
 
   if (article) {
+    console.log("Found article:", article);
     article.votes += votes;
     return Promise.resolve(article);
   } else {
+    console.error("Article not found");
     return Promise.resolve(null);
   }
 };
